8e5287f7471f37df109087abe7a63d015878e4b5
djpo312 <dannyparra312@gmail.com>
2026-07-10 08:25:47 -0500

    Primer commit - Proyecto Flutter

---
+++ android/app/src/main/kotlin/com/dannyparra/vida_asistente/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.dannyparra.vida_asistente
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/features/wod/wod_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WodPage extends StatelessWidget {
+  const WodPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        "Generador de WOD",
+        style: TextStyle(fontSize: 26),
+      ),
+    );
+  }
+}
