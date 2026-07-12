4f9955154422da52fe5e65581915c235f3bb7f62
djpo312 <dannyparra312@gmail.com>
2026-07-12 17:02:07 -0500

    Sprint 2 - Dashboard inicial

--- lib/features/dashboard/dashboard_page.dart
+++ lib/features/dashboard/dashboard_page.dart
@@ -6,17 +6,82 @@ class DashboardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Vida Asistente"),
-      ),
-      body: const Center(
-        child: Text(
-          "Bienvenido Danny 👋",
-          style: TextStyle(
-            fontSize: 28,
-            fontWeight: FontWeight.bold,
-          ),
+      appBar: AppBar(title: const Text("Vida Asistente")),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Hola Danny 👋",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+
+            const SizedBox(height: 8),
+
+            const Text(
+              "¿Qué quieres entrenar hoy?",
+              style: TextStyle(fontSize: 18, color: Colors.grey),
+            ),
+
+            const SizedBox(height: 25),
+
+            _menuCard(Icons.fitness_center, "CrossFit", Colors.orange),
+
+            _menuCard(Icons.directions_run, "Running", Colors.green),
+
+            _menuCard(Icons.local_fire_department, "Hyrox", Colors.red),
+
+            _menuCard(Icons.sports_gymnastics, "Gimnasio", Colors.blue),
+
+            const SizedBox(height: 25),
+
+            Card(
+              elevation: 3,
+              child: Padding(
+                padding: EdgeInsets.all(18),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Resumen semanal",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+
+                    SizedBox(height: 15),
+
+                    Text("🏋 Entrenamientos: 0"),
+
+                    Text("🔥 Calorías: 0"),
+
+                    Text("⏱ Tiempo: 0 min"),
+
+                    Text("🏆 PR nuevos: 0"),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
+      ),
+    );
+  }
+
+  Widget _menuCard(IconData icon, String title, Color color) {
+    return Card(
+      margin: const EdgeInsets.only(bottom: 15),
+      elevation: 4,
+      child: ListTile(
+        leading: CircleAvatar(
+          backgroundColor: color,
+          child: Icon(icon, color: Colors.white),
+        ),
+        title: Text(title, style: const TextStyle(fontWeight: FontWeight.bold)),
+        trailing: const Icon(Icons.arrow_forward_ios),
+        onTap: () {},
       ),
     );
   }
